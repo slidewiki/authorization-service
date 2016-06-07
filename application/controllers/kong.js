37361e7722bfb98190b6e8fddeea695f7be4bff4
Kurt Junghanns <tboonx@googlemail.com>
2016-06-07 10:40:55 +0200

    Added new methods to kong for api management.

--- application/controllers/kong.js
+++ application/controllers/kong.js
@@ -5,8 +5,7 @@ It has functions to create a costumer and a application and also to retrieve a a
 
 'use strict';
 
-const co = require('../common'),
-  request = require('request');
+const request = require('request');
 
 const KONG_ADMIN = 'http://localhost:8001/',
   KONG_OAUTH2 = 'http://testservice.localhost/oauth2/token';
@@ -27,8 +26,7 @@ module.exports = {
         if (!error && (response.statusCode === 201 || response.statusCode === 200)) {
           let consumer = JSON.parse(body);
           resolve(consumer);
-        }
-        else {
+        } else {
           reject(error);
         }
       }
@@ -55,8 +53,7 @@ module.exports = {
           if (consumerKongId !== application.consumer_id)
             reject('created Application for wrong consumer');
           resolve(application);
-        }
-        else {
+        } else {
           reject(error);
         }
       }
@@ -77,8 +74,7 @@ module.exports = {
         if (!error && response.statusCode === 201) {
           let authorization = JSON.parse(body);
           resolve(authorization);
-        }
-        else {
+        } else {
           reject(error);
         }
       }
@@ -90,4 +86,142 @@ module.exports = {
 
   //functions which are for a general configuration of Kongs API
 
+  //returns {"upstream_url":"","created_at":,"id":"","name":"","preserve_host":false,"strip_request_path":false,"request_host":""}
+  addUpstreamHost: (hostname, requestHost, upstreamURL) => {
+    let promise = new Promise((resolve, reject) => {
+      const options = {
+        url: KONG_ADMIN + 'apis/',
+        method: 'POST',
+        body: {
+          name: hostname,
+          request_host: requestHost,
+          upstream_url: upstreamURL
+        }
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode === 201) {
+          let api = JSON.parse(body);
+          if (api.name !== hostname)
+            reject('created Application for wrong consumer');
+          resolve(api);
+        } else {
+          reject(error);
+        }
+      }
+
+      request(options, callback);
+    });
+    return promise;
+  },
+
+  //returns nothing
+  deleteUpstreamHost: (hostId) => {
+    let promise = new Promise((resolve, reject) => {
+      const options = {
+        url: KONG_ADMIN + 'apis/' + hostId,
+        method: 'DELETE'
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode === 204) {
+          resolve(api);
+        } else {
+          reject(error);
+        }
+      }
+
+      request(options, callback);
+    });
+    return promise;
+  },
+
+  /*returns
+  {
+    "api_id": "",
+    "id": "",
+    "created_at": ,
+    "enabled": true,
+    "name": "",
+    "config": {
+      "mandatory_scope": false,
+      "token_expiration": 7200,
+      "enable_implicit_grant": false,
+      "hide_credentials": false,
+      "provision_key": "",
+      "accept_http_if_already_terminated": false,
+      "enable_authorization_code": true,
+      "enable_client_credentials": true,
+      "enable_password_grant": false
+    }
+  }
+  */
+  initializePluginOAuth2: (hostname, scopes) => {
+    let promise = new Promise((resolve, reject) => {
+      const options = {
+        url: KONG_ADMIN + 'apis/' + hostname + '/plugins',
+        method: 'POST',
+        body: {
+          name: 'oauth2',
+          'config.scopes': scopes,
+          'config.enable_client_credentials': true
+        }
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode === 201) {
+          let plugin = JSON.parse(body);
+          if (!plugin.enabled)
+            reject('Failed creating plugin');
+          resolve(plugin);
+        } else {
+          reject(error);
+        }
+      }
+
+      request(options, callback);
+    });
+    return promise;
+  },
+
+  /* returns
+  {
+    "id": ,
+    "api_id": "",
+    "consumer_id": "",
+    "name": "",
+    "config": {
+
+    },
+    "enabled": true,
+    "created_at":
+  }
+  */
+  initializePluginACL: (hostname, whitelist, blacklist) => {
+    let promise = new Promise((resolve, reject) => {
+      const options = {
+        url: KONG_ADMIN + 'apis/' + hostname + '/plugins',
+        method: 'POST',
+        body: {
+          name: 'acl',
+          'config.whitelist': whitelist,
+          'config.blacklist': blacklist
+        }
+      };
+
+      function callback(error, response, body) {
+        if (!error && response.statusCode === 201) {
+          let plugin = JSON.parse(body);
+          if (!plugin.enabled)
+            reject('Failed creating plugin');
+          resolve(plugin);
+        } else {
+          reject(error);
+        }
+      }
+
+      request(options, callback);
+    });
+    return promise;
+  }
 };
