b73a7f91ad1d760b36cc72cb7ec2b42b56a39e96
Ben Wulff <benjamin.wulff@iais.fraunhofer.de>
2017-06-13 04:25:20 +0200

    Fixing image building

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/authorization-service ./
+docker build -t slidewiki/authorization-service:latest-dev ./
 docker push slidewiki/authorization-service:latest-dev
